4260b7931834b68f760ab47cd635749ec96542c3
Nathan <63556914+PillageDev@users.noreply.github.com>
2025-01-24 16:25:31 -0500

    Added support for multiple limelights

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -4,15 +4,23 @@ import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.LimelightHelpers;
 
 public class Vision extends SubsystemBase {
-    private final String NAME = "limelight";
+    private static final String[] LIMELIGHT_NAMES = {
+        "front",
+        "back"
+    };
 
     public Vision() {
-        LimelightHelpers.setCameraPose_RobotSpace(NAME,
+        for (String name : LIMELIGHT_NAMES) {
+            setupLimelight(name);
+        }
+    }
+
+    private void setupLimelight(String limelightName) {
+        LimelightHelpers.setCameraPose_RobotSpace(limelightName,
             0.2, // forward
             0.0, // side
             0.19, // up
@@ -23,23 +31,29 @@ public class Vision extends SubsystemBase {
     }
 
     public void updateRobotPose(SwerveDrivePoseEstimator estimator, double gyroRate) {
+        if (Math.abs(gyroRate) > 720) {
+            return;
+        }
+        
+        for (String name : LIMELIGHT_NAMES) {
+            setVisionPoseEstimation(name, estimator);
+        }
+    }
+
+    private void setVisionPoseEstimation(String limelightName, SwerveDrivePoseEstimator estimator) {
         boolean rejectOdometry = false;
 
         LimelightHelpers.SetRobotOrientation(
-            NAME, 
+            limelightName, 
             estimator.getEstimatedPosition().getRotation().getDegrees(),
-            gyroRate,
+            0,
             0,
             0,
             0,
             0
         );
 
-        LimelightHelpers.PoseEstimate poseEstimate = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(NAME);
-
-        if (Math.abs(gyroRate) > 720) {
-            rejectOdometry = true;
-        }
+        LimelightHelpers.PoseEstimate poseEstimate = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
 
         if (poseEstimate.tagCount == 0) {
             rejectOdometry = true;  
@@ -47,11 +61,16 @@ public class Vision extends SubsystemBase {
 
         if (!rejectOdometry) {
             Pose2d visionPose = poseEstimate.pose;
+            Pose2d currentPose = estimator.getEstimatedPosition();
 
-            Logger.recordOutput("Limelight/EstimatedPose", visionPose);
+            // reject position greater than 1 meter apart from current
+            if (currentPose.getTranslation().getDistance(visionPose.getTranslation()) > 1) {
+                return;
+            }
+
+            Logger.recordOutput("Limelight/EstimatedPose-" + limelightName, visionPose);
 
             estimator.addVisionMeasurement(visionPose, poseEstimate.timestampSeconds);
         }
     }
-    
 }
